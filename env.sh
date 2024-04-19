6dd4613fc4a880a7937dd6e98ceb1aef7d039fef
Linzw <linzwcs@gmail.com>
2024-04-19 15:45:43 +0800

    update env.sh

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 #pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116 
-pip install datasets transformers nltk stanza evaluate scikit-learn spacy evaluate
+pip install datasets transformers==4.36.2 nltk stanza evaluate scikit-learn spacy evaluate
 pip install transformers[torch]
 pip install openai rankgen retriv sentencepiece sacremoses
 pip install Sentence-Transformers==2.2.2 
