28a12fa013cd12b9251bdad9e94b9fd630c2c381
Linzw <linzwcs@gmail.com>
2024-05-22 11:11:09 +0800

    add evaluation

---
+++ evaluation.py
@@ -0,0 +1,42 @@
+from detect import Detector
+import argparse
+from utils import jload
+from scipy.stats import pearsonr
+from sklearn.metrics import roc_auc_score, roc_curve
+from tqdm import tqdm
+
+# from sklearn import metrics
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_path", help="path to saved model")
+    parser.add_argument(
+        "--device", default="cuda", type=str, help="whether to train on gpu"
+    )
+    parser.add_argument(
+        "--golden_testfile",
+        help="test set with golden syntactic and lexical signals",
+    )
+    args = parser.parse_args()
+    detector = Detector(args.model_path, args.device)
+    data = jload(args.golden_testfile)
+    syntactic_label, lexical_label, labels, logits = [], [], [], []
+    for item in tqdm(data):
+        predictions = detector(item["text"])
+        logits += [p[1] for p in predictions]
+        syntactic_label += item["syntactic"]
+        lexical_label += item["lexical"]
+        labels += item["label"]
+    syntactic_corr, _ = pearsonr(logits, syntactic_label)
+    lexical_corr, _ = pearsonr(logits, lexical_label)
+    fpr, tpr, _ = roc_curve(labels, logits, pos_label=1)
+    metrics = {
+        "syntactic_corr": syntactic_corr,
+        "lexical_corr": lexical_corr,
+        "auc": roc_auc_score(labels, logits),
+        "Detection Accuracy": tpr[fpr < 0.01][-1],
+    }
+    # print("--metre")
+    for key, value in metrics.items():
+        print(f"{key}: {value}")
